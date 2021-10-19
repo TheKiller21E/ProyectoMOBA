8bfc5cc6fd4b7ff0fa56da5312e1f25cae6ae9e5
Freddy <freddy.ma35432@cesurformacion.com>
2021-10-19 12:57:26 +0200

    PLayer_minion
    movimiento asi minion

---
+++ Assets/Core/Code/CombatHero.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatHero : MonoBehaviour
+{
+    public GameObject TagetEnemy;
+    public enum HeroAttack { melee, ranged};
+    [SerializeField]
+    public HeroAttack heroAttack;
+
+    [SerializeField]
+    float attackranged;
+    [SerializeField]
+    float rotatespeedattack = 0.075f;
+
+    PlayerMovement2 movescript;
+
+    bool atckidle = false;
+    public bool alive;
+    [SerializeField]
+    bool performattack = true;
+
+    void Start()
+    {
+        movescript = GetComponent<PlayerMovement2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TagetEnemy != null)
+        {
+            if (Vector3.Distance(gameObject.transform.position,TagetEnemy.transform.position)>attackranged)
+            {
+                movescript.jugador.SetDestination(TagetEnemy.transform.position);
+                movescript.jugador.stoppingDistance = attackranged;
+
+                //Rotacion
+                Quaternion roationTolookAt = Quaternion.LookRotation(TagetEnemy.transform.position - transform.position);
+                float rotatey = Mathf.SmoothDampAngle(transform.eulerAngles.y, roationTolookAt.eulerAngles.y, ref movescript.rotatevelocity, rotatespeedattack * (Time.deltaTime * 5));
+                transform.eulerAngles = new Vector3(0, rotatey, 0);
+            }
+            else
+            {
+                if (heroAttack==HeroAttack.melee)
+                {
+                    if (performattack)
+                    {
+                        Debug.Log("ataque");
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Core/Code/PlayerMovement2.cs
+++ Assets/Core/Code/PlayerMovement2.cs
@@ -5,42 +5,63 @@ using UnityEngine.AI;
 
 public class PlayerMovement2 : MonoBehaviour
 {
-    NavMeshAgent jugador;
+    public NavMeshAgent jugador;
+
     [SerializeField]
-    float rotateSpeedmove = 0.075f;
-    float rotatevelocity;
-    private LayerMask mask;
+    public float rotateSpeedmove = 0.075f;
+    public float rotatevelocity;
+
+    //private LayerMask mask;
+
     public GameObject Loock;
+
+    CombatHero combatscript;
 
     void Start()
     {
-        mask = LayerMask.GetMask("Caminar");
+        combatscript = GetComponent<CombatHero>();
+       // mask = LayerMask.GetMask("Caminar");
         jugador = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (combatscript.TagetEnemy!=null)
+        {
+            if (combatscript.TagetEnemy.GetComponent<CombatHero>() != null)
+            {
+                if (!combatscript.TagetEnemy.GetComponent<CombatHero>().alive)
+                {
+                    combatscript.TagetEnemy = null;
+                }
+            }
+
+        }
+
         if (Input.GetMouseButtonDown(1)) //boton izquierdo
         {
             RaycastHit hit;
 
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, mask))
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity/*, mask*/))
             {
-                //Movimiento
-                jugador.SetDestination(hit.point);
+                if (hit.collider.tag == "floor")
+                {
+                    //Movimiento
+                    jugador.SetDestination(hit.point);
+                    combatscript.TagetEnemy = null;
+                    jugador.stoppingDistance = 0;
 
-                //Rotacion
-                Quaternion roationTolookAt = Quaternion.LookRotation(hit.point - transform.position);
-                float rotatey = Mathf.SmoothDampAngle(transform.eulerAngles.y, roationTolookAt.eulerAngles.y, ref rotatevelocity, rotateSpeedmove * (Time.deltaTime * 5));
+                    //Rotacion
+                    Quaternion roationTolookAt = Quaternion.LookRotation(hit.point - transform.position);
+                    float rotatey = Mathf.SmoothDampAngle(transform.eulerAngles.y, roationTolookAt.eulerAngles.y, ref rotatevelocity, rotateSpeedmove * (Time.deltaTime * 5));
+                    transform.eulerAngles = new Vector3(0, rotatey, 0);
 
-                //
-                transform.eulerAngles = new Vector3(0, rotatey, 0);
-
-                // el marcador
-                GameObject X;
-                X=Instantiate(Loock, hit.point,Loock.transform.rotation);
-                Destroy(X, 0.5f);
+                    // el marcador
+                    GameObject X;
+                    X = Instantiate(Loock, hit.point, Loock.transform.rotation);
+                    Destroy(X, 0.5f);
+                }
 
             }
         }
@@ -48,17 +69,20 @@ public class PlayerMovement2 : MonoBehaviour
         {
             RaycastHit hit;
 
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, mask))
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity/*, mask*/))
             {
-                //Movimiento
-                jugador.SetDestination(hit.point);
+                if (hit.collider.tag == "floor")
+                {
+                    //Movimiento
+                    jugador.SetDestination(hit.point);
+                    combatscript.TagetEnemy = null;
+                    jugador.stoppingDistance = 0;
 
-                //Rotacion
-                Quaternion roationTolookAt = Quaternion.LookRotation(hit.point - transform.position);
-                float rotatey = Mathf.SmoothDampAngle(transform.eulerAngles.y, roationTolookAt.eulerAngles.y, ref rotatevelocity, rotateSpeedmove * (Time.deltaTime * 5));
-
-                //
-                transform.eulerAngles = new Vector3(0, rotatey, 0);
+                    //Rotacion
+                    Quaternion roationTolookAt = Quaternion.LookRotation(hit.point - transform.position);
+                    float rotatey = Mathf.SmoothDampAngle(transform.eulerAngles.y, roationTolookAt.eulerAngles.y, ref rotatevelocity, rotateSpeedmove * (Time.deltaTime * 5));
+                    transform.eulerAngles = new Vector3(0, rotatey, 0);
+                }
             }
         }
     }
